3415d9d0e2def8e3846d9743937ddbf238907277
Barinder <barinder@oquant.com>
2020-06-21 23:09:32 -0700

    Package Files (0.8)

---
+++ R/PackageFNN.R
@@ -0,0 +1,6 @@
+#' @details
+#' You can list off functions you wish to link to (maybe all of the functions in your package)
+#' using the bracket syntax: [colSums()] for example.  `Type like this` to get console font.
+#' @keywords internal
+"_PACKAGE"
+#> [1] "_PACKAGE"
